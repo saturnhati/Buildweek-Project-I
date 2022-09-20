894af0315e5fea04a40e1a914371e1b661a98e8b
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-09-20 18:12:57 +0200

    fixed menu hamburger on scroll

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 // HEADER
+let menuOpen = false
 
 // position fixed nav bar
 let colla = document.querySelector('#colla')
 
 document.addEventListener('scroll', function () {
     let top = window.pageYOffset
-    let clientHeight = document.querySelector('header').clientHeight;
+    let burgerNotColla = document.getElementById('hamburger_menu')
+    let clientHeight = document.querySelector('header').clientHeight
     let calc = (clientHeight - 85)
     if (top > calc) {
         colla.style.position = 'fixed';
         colla.style.marginTop = 0;
         colla.style.top = 0;
+        if (menuOpen == true) {
+            burgerNotColla.style.display = 'none';
+        }
     }
     else {
         colla.style.position = 'absolute';
         colla.style.marginTop = '-85px';
         colla.style.top = 'unset';
+        if (menuOpen == true) {
+            burgerNotColla.style.display = 'block';
+        }
     }
 })
 
 // hamburger menu
 let width = window.innerWidth
-let menuOpen = false
-
-// addEventListener('resize', (event) => {});
-
-// onresize = (event) => {};
-
 
 function burgerMenu() {
     let burger = document.getElementById('hamburger_menu')
